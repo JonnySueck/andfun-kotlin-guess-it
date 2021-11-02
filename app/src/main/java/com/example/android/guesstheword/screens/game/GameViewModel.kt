67d2a255c1d67f800ec18e0fad8bbae16a17881e
Jonathan Sueck <jonathansueck@gmail.com>
2021-11-02 14:54:43 -0400

    added LiveData into the GameViewModel to make the score unmutable outside of the viewmodel

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -17,16 +17,21 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 
 class GameViewModel : ViewModel(){
     // The current mutable word
-    val word = MutableLiveData<String>()
+    private val _word = MutableLiveData<String>()
+    val word : LiveData<String>
+        get() = _word
 
     // The current mutable score
-    var score = MutableLiveData<Int>()
+    private val _score = MutableLiveData<Int>()
+    val score : LiveData<Int>
+        get() = _score
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
@@ -36,7 +41,7 @@ class GameViewModel : ViewModel(){
         Log.i("GameViewModel", "GameViewModel Created!")
         resetList()
         nextWord()
-        score.value = 0
+        _score.value = 0
     }
     /**
      * Resets the list of words and randomizes the order
@@ -76,17 +81,17 @@ class GameViewModel : ViewModel(){
         if (wordList.isEmpty()) {
             // gameFinished()
         } else {
-            word.value = wordList.removeAt(0)
+            _word.value = wordList.removeAt(0)
         }
     }
 //  check to see if score has a null value -> (score.value)?
     fun onSkip() {
-        score.value = (score.value)?.minus( 1)
+        _score.value = (score.value)?.minus( 1)
         nextWord()
     }
 
     fun onCorrect() {
-        score.value = (score.value)?.plus(1)
+        _score.value = (score.value)?.plus(1)
         nextWord()
     }
 
